df184dbbfa656d2a8bbb4af5806f130858ec460b
Kevin Grieger <scuffmark@gmail.com>
2016-02-13 13:52:55 -0600

    saving layouts works

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,9 +23,10 @@
 
   //this should be a service
   myLayout.registerComponent( 'example',function( container, state ){
+    console.log(state)
     var editor = ace.edit(container.getElement()[0]);
     editor.setTheme(state.theme || 'ace/theme/monokai');
-    editor.session.setMode(state.mode || 'javascript');
+    editor.session.setMode( 'ace/mode/'+(state.mode || 'javascript'));
     editor.setOptions({fontSize: state.size || '14px'});
     container.setTitle(state.name || 'untitled');
 
